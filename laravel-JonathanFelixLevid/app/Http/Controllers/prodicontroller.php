cf2b8a8aa9fc3fa7c0c51ff421095354e878e924
JonathanF123 <146701193+JonathanF123@users.noreply.github.com>
2023-11-06 13:43:00 +0700

    update

--- laravel-JonathanFelixLevid/app/Http/Controllers/prodicontroller.php
+++ laravel-JonathanFelixLevid/app/Http/Controllers/prodicontroller.php
@@ -10,4 +10,10 @@ class prodicontroller extends Controller
         $kampus = "Universitas MDP";
         return view("prodi.index")->with('KAMPUS', $kampus);
     }
+
+    public function allJoinFacade(){
+        $kampus = "Universitas Multi Data Palembang";
+        $result = DB::select('select mahasiswas.*, prodis.nama as nama_prodi from prodis, mahasiswas where prodis.id = mahasiswas.prodi_id');
+        return view('prodi.index')->with('allmahasiswaprodi' $result, 'kampus' => $kampus);
+    }
 }
--- laravel-JonathanFelixLevid/database/seeders/DatabaseSeeder.php
+++ laravel-JonathanFelixLevid/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Prodi;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +20,20 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+            Prodi::Create(
+                [
+                    'nama' => 'Teknik Informatika'
+                ]
+            );
+            Prodi::Create(
+                [
+                    'nama' => 'Mananjemen Informatika'
+                ]
+            );
+            Prodi::Create(
+                [
+                    'nama' => 'Sistem Informasi'
+                ]
+            );
     }
 }
